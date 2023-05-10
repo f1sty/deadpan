5f30be1cb3f05191427c51a96e3271fb9d6e9c11
Yurii Skrynnykov <truef1s7@gmail.com>
2023-05-10 22:32:50 +0300

    Add free space slot

--- config.h
+++ config.h
@@ -1,5 +1,9 @@
+#include <sys/statvfs.h>
 #define DATE_FORMAT "%F %H:%M:%S"
+#define MOUNT_POINT "/"
 #define INTERVAL 1
+#define DELIMITER " | "
 
 void datetime_str(char *dt_str);
+void free_space_str(char *fs_str);
 void run(char *cmd[]);
--- main.c
+++ main.c
@@ -2,15 +2,27 @@
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/statvfs.h>
 #include <time.h>
 #include <unistd.h>
 
+static const long space_divider = 1024 * 1024 * 1024;
+
 int main(int argc, char *argv[]) {
-  char *buf = (char *)malloc(sizeof(char) * 100);
+  char *datetime = (char *)malloc(sizeof(char) * 50);
+  char *free_space = (char *)malloc(sizeof(char) * 20);
+  // size should be size of all components + num of components
+  char *status = (char *)malloc(sizeof(char) * (70 + 2));
 
   while (1) {
-    datetime_str(buf);
-    char *update_cmd[] = {"/usr/bin/xsetroot", "-name", buf, NULL};
+    datetime_str(datetime);
+    free_space_str(free_space);
+
+    strcpy(status, free_space);
+    strcat(status, datetime);
+
+    char *update_cmd[] = {"/usr/bin/xsetroot", "-name", status, NULL};
     pid_t pid = fork();
 
     // Ignore dead childs so we won't create lots of zombies!
@@ -25,7 +37,9 @@ int main(int argc, char *argv[]) {
       exit(EXIT_FAILURE);
     case 0:
       run(update_cmd);
-      free(buf);
+      free(status);
+      free(free_space);
+      free(datetime);
       exit(EXIT_SUCCESS);
     default:
       sleep(INTERVAL);
@@ -35,11 +49,22 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-void datetime_str(char *buf) {
+void datetime_str(char *datetime) {
   time_t current_time = time(NULL);
   struct tm *tm = localtime(&current_time);
 
-  strftime(buf, 99, DATE_FORMAT, tm);
+  strftime(datetime, 100, DATE_FORMAT, tm);
+}
+
+void free_space_str(char *fs_str) {
+  struct statvfs fs_stats;
+  if (statvfs(MOUNT_POINT, &fs_stats) == -1) {
+    perror("statvfs");
+    exit(EXIT_FAILURE);
+  }
+  // getting free space in bytes
+  long free_space = (fs_stats.f_bavail * fs_stats.f_bsize) / space_divider;
+  snprintf(fs_str, 20, "%ld Gb%s", free_space, DELIMITER);
 }
 
 void run(char *cmd[]) {
