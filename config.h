8d371a3e8f7d7e134ca0ddd87192e1bf1ebf7366
Yurii Skrynnykov <truef1s7@gmail.com>
2023-05-11 13:46:42 +0300

    Add help on how to choose thermal zone

--- config.h
+++ config.h
@@ -3,6 +3,9 @@
 #define MOUNT_POINT "/"
 #define INTERVAL 1
 #define DELIMITER " | "
+// to get right thermal zone number for CPU temp do
+// `cat /sys/class/thermal/thermal_zone<THERMAL_ZONE>/type`
+// You are looking for `x86_pkg_temp` type.
 #define THERMAL_ZONE 2
 
 void datetime_str(char *dt_str);
