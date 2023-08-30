53a3dc02e15129614e79fd8422613f573efaef1d
Yurii Skrynnykov <truef1s7@gmail.com>
2023-08-30 21:43:11 +0300

    Change thermal zone to correct

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
 // to get right thermal zone number for CPU temp do
 // `cat /sys/class/thermal/thermal_zone<THERMAL_ZONE>/type`
 // You are looking for `x86_pkg_temp` type.
-#define THERMAL_ZONE 2
+#define THERMAL_ZONE 1
 #define VOLUME_CMD "volume.sh"
 
 void date_time_str(char *dt_str);
